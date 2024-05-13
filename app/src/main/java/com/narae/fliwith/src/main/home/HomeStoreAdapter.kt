3544cf49c516f70a64037c4b036af96faa3c6195
Eunice991217 <101406317+Eunice991217@users.noreply.github.com>
2024-05-13 23:44:20 +0900

    refactor : 상태바 색상 변경

--- app/src/main/java/com/narae/fliwith/src/main/home/HomeStoreAdapter.kt
+++ app/src/main/java/com/narae/fliwith/src/main/home/HomeStoreAdapter.kt
@@ -15,6 +15,7 @@ class HomeStoreAdapter (private val storeDataList: MutableList<Store>) : Recycle
         fun bindInfo(tour: Store) {
             binding.apply {
                 storeImg.setImageResource(R.drawable.store_default_image)
+                storeName.text = "식당1"
                 storeImg.clipToOutline = true
             }
         }
