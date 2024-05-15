1fcade6a369e76e07ff625c194538543632abf7a
bulbasaur232 <rdd2033@naver.com>
2024-05-16 01:34:32 +0900

    feat : MyPageFragment 구현 완료, 프로필 안내 Dialog 추가 예정

--- app/src/main/java/com/narae/fliwith/src/auth/AuthActivity.kt
+++ app/src/main/java/com/narae/fliwith/src/auth/AuthActivity.kt
@@ -1,12 +1,17 @@
 package com.narae.fliwith.src.auth
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.narae.fliwith.R
 
 class AuthActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        val tv = findViewById<TextView>(R.id.username);
+        tv.currentTextColor
     }
 }
