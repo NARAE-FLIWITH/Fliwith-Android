85a5e69f5db971825cccf243a748f7da98a6b95b
bulbasaur232 <rdd2033@naver.com>
2024-05-16 21:59:04 +0900

    feat : splash screen 구현 완료

---
+++ app/src/main/java/com/narae/fliwith/src/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.narae.fliwith.src.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.narae.fliwith.R
+import com.narae.fliwith.config.BaseActivity
+import com.narae.fliwith.databinding.ActivitySplashBinding
+import com.narae.fliwith.src.auth.AuthActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, AuthActivity::class.java))
+            finish()
+        }, 1500)
+
+        setContentView(R.layout.activity_splash)
+    }
+}
