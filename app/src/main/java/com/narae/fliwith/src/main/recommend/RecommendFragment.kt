45f083309cd7b04a2e8ab4898b9bb31a1d87e873
Eunice991217 <101406317+Eunice991217@users.noreply.github.com>
2024-05-16 10:53:36 +0900

    feat : AI 추천 페이지 완료

--- app/src/main/java/com/narae/fliwith/src/main/recommend/RecommendFragment.kt
+++ app/src/main/java/com/narae/fliwith/src/main/recommend/RecommendFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.narae.fliwith.R
+import com.narae.fliwith.databinding.FragmentRecommendBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 class RecommendFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
+
+    private var _binding : FragmentRecommendBinding? = null
+    private val binding
+        get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +30,18 @@ class RecommendFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recommend, container, false)
+        _binding = FragmentRecommendBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.regionBtn.layoutSelectAiText.text = "지역"
+        binding.tourBtn.layoutSelectAiText.text = "여행지"
+        binding.selectDisableBtn.layoutSelectAiText.text = "장애 선택"
+        binding.memberBtn.layoutSelectAiText.text = "인원 수"
+        binding.dateBtn.layoutSelectAiText.text = "날짜"
     }
 
     companion object {
