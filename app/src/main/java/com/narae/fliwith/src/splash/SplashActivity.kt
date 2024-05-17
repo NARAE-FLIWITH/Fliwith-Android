8f5507463291bfe5f8ebfcaa4bcd3dcbb14fa2e5
bulbasaur232 <rdd2033@naver.com>
2024-05-17 09:37:46 +0900

    fix : 상태바가 사라지지 않는 문제 수정

--- app/src/main/java/com/narae/fliwith/src/splash/SplashActivity.kt
+++ app/src/main/java/com/narae/fliwith/src/splash/SplashActivity.kt
@@ -2,23 +2,47 @@ package com.narae.fliwith.src.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowManager
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.narae.fliwith.config.BaseActivity
 import com.narae.fliwith.databinding.ActivitySplashBinding
 import com.narae.fliwith.src.auth.AuthActivity
+
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setFullScreen()
+        delaySplash(1500)
+    }
 
+    private fun delaySplash(delayMillis: Long) {
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, AuthActivity::class.java))
             finish()
-        }, 1500)
+        }, delayMillis)
+    }
 
-        setContentView(binding.root)
+    private fun setFullScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            window.apply {
+                setFlags(
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                )
+            }
+        } else {
+            val windowInsetsController = WindowInsetsControllerCompat(
+                window, window.decorView
+            )
+            windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        }
     }
 }
