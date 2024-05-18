f74af812c54d111c17b5fbf2d77802495643c055
bulbasaur232 <rdd2033@naver.com>
2024-05-18 22:35:49 +0900

    feat : 회원가입 완료 화면 추가

---
+++ app/src/main/java/com/narae/fliwith/src/auth/signup/CompleteFragment.kt
@@ -0,0 +1,13 @@
+package com.narae.fliwith.src.auth.signup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.narae.fliwith.R
+import com.narae.fliwith.config.BaseFragment
+import com.narae.fliwith.databinding.FragmentCompleteBinding
+
+class CompleteFragment : BaseFragment<FragmentCompleteBinding>(FragmentCompleteBinding::inflate) {
+}
