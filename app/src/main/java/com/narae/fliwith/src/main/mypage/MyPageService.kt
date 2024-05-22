a09db89d8067a395d25361202a50d5d0d1119387
bulbasaur232 <rdd2033@naver.com>
2024-05-22 15:07:40 +0900

    feat : 마이페이지 프로필 세팅 완료

--- app/src/main/java/com/narae/fliwith/src/main/mypage/MyPageService.kt
+++ app/src/main/java/com/narae/fliwith/src/main/mypage/MyPageService.kt
@@ -2,13 +2,18 @@ package com.narae.fliwith.src.main.mypage
 
 import com.narae.fliwith.config.ApplicationClass
 import com.narae.fliwith.config.models.ResponseDto
+import com.narae.fliwith.src.main.mypage.models.ProfileResponse
 import retrofit2.Response
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface MyPageService {
 
     @POST("user/logout")
     suspend fun logout(): Response<ResponseDto>
+
+    @GET("user/profile")
+    suspend fun getProfile() : Response<ProfileResponse>
 }
 
 object MyPageApi {
---
+++ app/src/main/java/com/narae/fliwith/src/main/mypage/models/Profile.kt
@@ -0,0 +1,8 @@
+package com.narae.fliwith.src.main.mypage.models
+
+import com.narae.fliwith.util.DISABILITY
+
+data class Profile(
+    val disability: DISABILITY,
+    val nickname: String
+)
