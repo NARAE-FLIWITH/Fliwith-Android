1d10b4ef0f93a2c6664187ea71bf365e1a548f6e
권오윤 <rdd2033@naver.com>
2024-07-27 15:18:57 +0900

    fix : 뒤로가기 버튼 크기 통일

---
+++ app/src/main/java/com/narae/fliwith/src/auth/login/ResetPasswordFragment.kt
@@ -0,0 +1,14 @@
+package com.narae.fliwith.src.auth.login
+
+import android.os.Bundle
+import android.view.View
+import com.narae.fliwith.config.BaseFragment
+import com.narae.fliwith.databinding.FragmentResetPasswordBinding
+
+
+class ResetPasswordFragment :
+    BaseFragment<FragmentResetPasswordBinding>(FragmentResetPasswordBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
