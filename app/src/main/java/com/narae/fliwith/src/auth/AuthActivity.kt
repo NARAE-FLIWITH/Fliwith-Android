ad4a21e814ed5f3387619b777dfc69ccbd180fc0
권오윤 <rdd2033@naver.com>
2024-07-27 14:31:33 +0900

    feat : 키보드 입력시 화면 바깥 부분 터치하면 키보드 내려가도록 변경

--- app/src/main/java/com/narae/fliwith/src/auth/AuthActivity.kt
+++ app/src/main/java/com/narae/fliwith/src/auth/AuthActivity.kt
@@ -1,6 +1,8 @@
 package com.narae.fliwith.src.auth
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import com.narae.fliwith.config.BaseActivity
 import com.narae.fliwith.databinding.ActivityAuthBinding
@@ -12,5 +14,12 @@ class AuthActivity : BaseActivity<ActivityAuthBinding>(ActivityAuthBinding::infl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        val imm: InputMethodManager =
+            getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        return super.dispatchTouchEvent(ev)
     }
 }
