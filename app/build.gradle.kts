3233c9a7027ac66c6c12037dc70aeefdb510b232
김민경 <org9899@naver.com>
2024-08-25 22:33:26 +0900

    feat : 회원탈퇴 임시로 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.narae.fliwith"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "SERVER_URL", getProperty("SERVER_URL"))
--- app/src/main/java/com/narae/fliwith/src/main/mypage/MyPageDetailFragment.kt
+++ app/src/main/java/com/narae/fliwith/src/main/mypage/MyPageDetailFragment.kt
@@ -1,23 +1,34 @@
 package com.narae.fliwith.src.main.mypage
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import com.kakao.sdk.auth.AuthApiClient
+import com.kakao.sdk.user.UserApiClient
 import com.narae.fliwith.R
 import com.narae.fliwith.config.BaseFragment
 import com.narae.fliwith.databinding.DialogProfileGuideBinding
 import com.narae.fliwith.databinding.FragmentMyPageDetailBinding
+import com.narae.fliwith.src.auth.AuthActivity
+import com.narae.fliwith.src.main.LoginViewModel
 import com.narae.fliwith.src.main.mypage.models.Profile
 import com.narae.fliwith.util.DISABILITY
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
+private const val TAG = "MyPageDetailFragment_싸피"
 class MyPageDetailFragment :
     BaseFragment<FragmentMyPageDetailBinding>(FragmentMyPageDetailBinding::inflate) {
     private val profileViewModel by activityViewModels<ProfileViewModel>()
+    private val loginViewModel by activityViewModels<LoginViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -53,6 +64,29 @@ class MyPageDetailFragment :
 
         binding.layoutPw.setOnClickListener {
             navController.navigate(R.id.action_myPageDetailFragment_to_changePasswordFragment)
+        }
+
+        binding.layoutResign.setOnClickListener {
+            lifecycleScope.launch {
+                val response = withContext(Dispatchers.IO) { MyPageApi.myPageService.logout() }
+                // 카카오 로그아웃
+                if (AuthApiClient.instance.hasToken()) {
+                    UserApiClient.instance.logout { error ->
+                        if (error != null) {
+                            Log.e(TAG, "로그아웃 실패. SDK에서 토큰 삭제됨", error)
+                        } else {
+                            Log.i(TAG, "로그아웃 성공. SDK에서 토큰 삭제됨")
+                        }
+                    }
+                }
+
+                loginViewModel.logout()
+                val intent = Intent(requireContext(), AuthActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                }
+                startActivity(intent)
+
+            }
         }
     }
 
