5fd91cb05387d453b78726dcf104d902c87daa25
김민경 <org9899@naver.com>
2024-08-31 23:08:04 +0900

    fix : 배포 version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.narae.fliwith"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "SERVER_URL", getProperty("SERVER_URL"))
